816e18e21255944971e732672b838ee5ec8c2516
pbagri45 <pbagri@rrc.ca>
2025-03-26 03:29:18 -0500

    Validation in Province

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,8 @@
 class Province < ApplicationRecord
-end
+    has_many :users
+    has_many :orders
+  
+    validates :name, :code, presence: true
+    validates :gst_rate, :pst_rate, :hst_rate, 
+              numericality: { greater_than_or_equal_to: 0 }
+  end
