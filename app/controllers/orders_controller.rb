e0c1bac66908cadea40bdf9b211c5b0cfce6cd2a
pbagri45 <pbagri@rrc.ca>
2025-03-26 03:50:01 -0500

    Controller orders updt

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_cart_items, only: [:new, :create]
+  before_action :set_order, only: [:show]
+
   def new
+    @order = current_user.orders.build
+    @provinces = Province.all
+    @subtotal = calculate_subtotal(@cart_items)
   end
 
   def create
-  end
+    @order = current_user.orders.build(order_params)
+    @order.status = 'pending'
+    set_order_totals(@order)
 
-  def show
+    if @order.save
+      create_order_items(@order)
+      process_payment(@order)
+    else
+      @provinces = Province.all
+      @subtotal = calculate_subtotal(@cart_items)
+      render :new
+    end
   end
 
   def index
+    @orders = current_user.orders.order(created_at: :desc)
   end
+
+  def show
+    @order_items = @order.order_items.includes(:product)
+  end
+
+  private
+
+  # ... (private methods for order processing)
 end
