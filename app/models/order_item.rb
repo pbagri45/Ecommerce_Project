433bad8bf36ffd218e638fa9c119aa18915ce151
pbagri45 <pbagri@rrc.ca>
2025-03-26 03:33:40 -0500

    Validations in Order_Item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,15 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+  validates :price_at_purchase, numericality: { greater_than: 0 }
+
+  before_validation :set_price_at_purchase
+
+  private
+
+  def set_price_at_purchase
+    self.price_at_purchase ||= product.current_price
+  end
 end
