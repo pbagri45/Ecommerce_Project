39b2ab7be4709a575c5f3b145d37ab91e84541a7
pbagri45 <pbagri@rrc.ca>
2025-03-26 03:32:37 -0500

    Validations in Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :province, optional: true
+  has_many :orders
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :address, :city, :postal_code, presence: true, if: :province_id?
 end
