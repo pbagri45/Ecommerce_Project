a16189a91bf9d247daf252864e887fdd3c3e6c84
pbagri45 <pbagri@rrc.ca>
2025-03-26 03:34:04 -0500

    Validations in Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,12 @@
 class Page < ApplicationRecord
-end
+    validates :title, :slug, :content, presence: true
+    validates :slug, uniqueness: true
+  
+    before_validation :generate_slug
+  
+    private
+  
+    def generate_slug
+      self.slug ||= title.parameterize if title
+    end
+  end
